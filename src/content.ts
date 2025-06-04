903ab3b74a1bf40c99c704637d017b2e2bb61f05
Daniel <daniel.song929@gmail.com>
2025-06-04 17:19:36 +1000

    able to get best move from stockfish api, and find best piece element

--- src/content.ts
+++ src/content.ts
@@ -1,5 +1,7 @@
 import { Chess } from "chess.js";
 
+let side = 0;
+let squareDim = 0;
 async function waitForBoard(): Promise<HTMLElement> {
   while (true) {
     const board = document.querySelector("cg-board") as HTMLElement | null;
@@ -17,6 +19,49 @@ async function waitForRM6(): Promise<HTMLElement> {
     }
     await new Promise((resolve) => setTimeout(resolve, 500)); // Wait 0.5 seconds
   }
+}
+
+async function getBestMove(fen: string, depth: number = 15) {
+  const url = `https://stockfish.online/api/s/v2.php?fen=${encodeURIComponent(
+    fen
+  )}&depth=${depth}`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("API error:", error);
+    return null;
+  }
+}
+
+function getPieceAtCoord(coord: string): HTMLElement | undefined {
+  const leftFile = side === 0 ? "a" : "h";
+  const file = coord[0];
+  const rank = parseInt(coord[1]);
+  const x =
+    side === 0
+      ? file.charCodeAt(0) - leftFile.charCodeAt(0)
+      : leftFile.charCodeAt(0) - file.charCodeAt(0);
+  const y = side === 0 ? 8 - rank : rank - 1;
+  console.log(`${x},${y}`);
+  const pieces = Array.from(document.querySelectorAll("piece"));
+  return pieces.find((piece) => {
+    const pieceEl = piece as HTMLElement;
+    const transformXY = pieceEl.style.transform.match(
+      /translate\(([^,]+), ([^)]+)\)/
+    );
+    if (transformXY) {
+      const xPx = parseInt(transformXY[1]);
+      const yPx = parseInt(transformXY[2]);
+      if (x * squareDim === xPx && y * squareDim === yPx) {
+        return piece;
+      }
+    }
+  }) as HTMLElement | undefined;
 }
 function blockMove(event: MouseEvent, square: HTMLElement) {
   return;
@@ -41,9 +86,7 @@ function blockMove(event: MouseEvent, square: HTMLElement) {
             const move = nodeEl.textContent?.trim();
             if (move) {
               const validMove = chessjs.move(move);
-              if (validMove) {
-                console.log(chessjs.fen());
-              } else {
+              if (!validMove) {
                 console.error("Invalid move...");
               }
             }
@@ -78,8 +121,19 @@ function blockMove(event: MouseEvent, square: HTMLElement) {
       movelistObserver.observe(l4x, { childList: true });
     }
     const board = await waitForBoard();
-    const cgWrap = document.querySelector(".cg-wrap");
+    squareDim = board.offsetHeight / 8;
+    function updateSquareDim() {
+      squareDim = board.offsetHeight / 8;
+    }
+    window.addEventListener("resize", updateSquareDim);
+    window.addEventListener("beforeunload", () => {
+      window.removeEventListener("resize", updateSquareDim);
+    });
+    const cgWrap = document.querySelector(".cg-wrap") as
+      | HTMLElement
+      | undefined;
     if (!cgWrap) return;
+    side = cgWrap.classList.contains("orientation-white") ? 0 : 1;
     const yourTurnContainer = document.querySelector(
       "div.rclock.rclock-turn.rclock-bottom"
     ) as HTMLElement;
@@ -87,9 +141,13 @@ function blockMove(event: MouseEvent, square: HTMLElement) {
 
     // ALL GOOD TO GO
 
+    let bestMove = null;
+    let bestMovePiece: HTMLElement | undefined = undefined;
+    let bestMoveSquare: HTMLElement | undefined = undefined;
+    if (bestMoveSquare) console.log(bestMoveSquare);
     let selectedEl: HTMLElement | undefined = undefined;
     const moveDests = new Set<HTMLElement>();
-    const yourTurnObserver = new MutationObserver((mutationsList) => {
+    const yourTurnObserver = new MutationObserver(async (mutationsList) => {
       if (!l4x) {
         l4x = rm6.querySelector("l4x") as HTMLElement | undefined;
         if (l4x) {
@@ -101,9 +159,7 @@ function blockMove(event: MouseEvent, square: HTMLElement) {
               if (move) {
                 console.log(move);
                 const validMove = chessjs.move(move);
-                if (validMove) {
-                  console.log(chessjs.fen());
-                } else {
+                if (!validMove) {
                   console.error("Invalid move...");
                 }
               }
@@ -111,7 +167,20 @@ function blockMove(event: MouseEvent, square: HTMLElement) {
           });
         }
       }
-      if (mutationsList[0].addedNodes.length === 1) console.log("Your turn!");
+      if (mutationsList[0].addedNodes.length === 1) {
+        // User's turn
+        bestMove = await getBestMove(chessjs.fen());
+        const bestMoveInLan = bestMove.bestmove.split(" ")[1];
+        console.log(bestMoveInLan);
+        const bestMoveStart = bestMoveInLan.slice(0, 2);
+        const bestMoveEnd = bestMoveInLan.slice(2, 4);
+        const promotedPiece =
+          bestMoveInLan.length === 5 ? bestMoveInLan[4] : undefined;
+        console.log(bestMoveEnd);
+        if (promotedPiece) console.log(promotedPiece);
+        bestMovePiece = getPieceAtCoord(bestMoveStart);
+        console.log(bestMovePiece);
+      }
     });
     yourTurnObserver.observe(yourTurnContainer, { childList: true });
     const observer = new MutationObserver((mutationsList) => {
